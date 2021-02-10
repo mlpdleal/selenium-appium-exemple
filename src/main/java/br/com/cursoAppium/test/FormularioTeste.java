e39c37f0481f047680e373ea79559cac03832a85
mlpdleal <manoellealdeveloper@gmail.com>
2021-02-10 15:35:49 -0300

    Incluindo DSL

--- src/main/java/br/com/cursoAppium/test/FormularioTeste.java
+++ src/main/java/br/com/cursoAppium/test/FormularioTeste.java
@@ -1,104 +1,52 @@
 package br.com.cursoAppium.test;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import static org.junit.Assert.assertEquals;
 
+import java.net.MalformedURLException;
+
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
+import br.com.cursoAppium.core.DSL;
+import br.com.cursoAppium.core.DriverFactory;
 import io.appium.java_client.MobileBy;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
 
 public class FormularioTeste {
 
+	private DSL dsl = new DSL();
+
+	@Before
+	public void setUp() throws MalformedURLException {
+		dsl.clicarPorTexto("Formulário");
+	}
+
+	@After
+	public void tearDown() {
+		DriverFactory.killDriver();
+	}
+
 	@Test
 	public void devePreencherCampoTexto() throws MalformedURLException {
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		desiredCapabilities.setCapability("platformName", "Android");
-		desiredCapabilities.setCapability("deviceName", "emulator-5554");
-		desiredCapabilities.setCapability("automationName", "uiautomator2");
-		desiredCapabilities.setCapability(MobileCapabilityType.APP,
-				"/home/manoel/Documents/automation_projects/selenium-appium-exemple/src/main/resources/CTAppium_1_2(1).apk");
-
-		AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),
-				desiredCapabilities);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-
-		List<MobileElement> elementosEncontrados = driver.findElements(By.className("android.widget.TextView"));
-		elementosEncontrados.get(1).click();
-
-		MobileElement campoNome = driver.findElement(MobileBy.AccessibilityId("nome"));
-		campoNome.sendKeys("Manoel");
-
-		String text = campoNome.getText();
-		Assert.assertEquals("Manoel", text);
-
-		driver.quit();
-
+		dsl.escrever(MobileBy.AccessibilityId("nome"), "Manoel");
+		assertEquals("Manoel", dsl.obterTexto(MobileBy.AccessibilityId("nome")));
 	}
-	
-	
+
 	@Test
 	public void deveInteragirComOCombo() throws MalformedURLException {
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		desiredCapabilities.setCapability("platformName", "Android");
-		desiredCapabilities.setCapability("deviceName", "emulator-5554");
-		desiredCapabilities.setCapability("automationName", "uiautomator2");
-		desiredCapabilities.setCapability(MobileCapabilityType.APP,
-				"/home/manoel/Documents/automation_projects/selenium-appium-exemple/src/main/resources/CTAppium_1_2(1).apk");
-
-		AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),
-				desiredCapabilities);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		driver.findElement(By.xpath("//android.widget.TextView[@text='Formulário']")).click();
-		driver.findElement(MobileBy.AccessibilityId("console")).click();
-		driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='Nintendo Switch']")).click();
-		
-		String texto = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
-		
+		dsl.selecionarCombo(MobileBy.AccessibilityId("console"), "Nintendo Switch");
+		String texto = dsl.obterTexto(By.xpath("//android.widget.Spinner/android.widget.TextView"));
 		Assert.assertEquals("Nintendo Switch", texto);
-		
-		driver.quit();
-
 	}
-	
+
 	@Test
 	public void deveInteragirCheckBox() throws MalformedURLException {
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-		desiredCapabilities.setCapability("platformName", "Android");
-		desiredCapabilities.setCapability("deviceName", "emulator-5554");
-		desiredCapabilities.setCapability("automationName", "uiautomator2");
-		desiredCapabilities.setCapability(MobileCapabilityType.APP,
-				"/home/manoel/Documents/automation_projects/selenium-appium-exemple/src/main/resources/CTAppium_1_2(1).apk");
-
-		AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),
-				desiredCapabilities);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		driver.findElement(By.xpath("//*[@text='Formulário']")).click();
-		
-		MobileElement check = driver.findElement(MobileBy.AccessibilityId("check"));
-		
-		check.click();
-		
-		MobileElement switchElement = driver.findElement(MobileBy.AccessibilityId("switch"));
-		
-		switchElement.click();
-		
-		Assert.assertEquals("true",check.getAttribute("checked"));
-		Assert.assertEquals("false", switchElement.getAttribute("checked"));
-		
-		driver.quit();
-
+		dsl.clicar(MobileBy.AccessibilityId("check"));
+		dsl.clicar(MobileBy.AccessibilityId("switch"));
+		Assert.assertTrue(dsl.isCheckMarcado(MobileBy.AccessibilityId("check")));
+		Assert.assertFalse(dsl.isCheckMarcado(MobileBy.AccessibilityId("switch")));
 	}
-	
 
 }
